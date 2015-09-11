f55369bee2346e40f4130a654b5aa10323230481
Nora Top <top.nora@gmail.com>
2015-09-11 21:14:38 +0000

    add factorial function

--- answers.js
+++ answers.js
@@ -1,31 +1,31 @@
 // returns the first character of the string 
 function FirstChar(str) {
     if (typeof str === 'string') {return str[0];}
-    else {console.log("Please provide a string as input");}
+    else {return "Please provide a string as input";}
 };
 
 // returns the last character of a string
 function LastChar(str) {
     if (typeof str === 'string') {return str[str.length - 1];}
-    else {console.log("Please provide a string as input");}
+    else {return "Please provide a string as input";}
 };
 
 // returns the character at the position represented by the number
 function charPosition(str,num) {
     if (typeof str === 'string' && typeof num === 'number'){return str[num];}
-    else {console.log("Please provide a string as the first input and a number as a second input");}
+    else {return "Please provide a string as the first input and a number as a second input";}
 }
 
 // return the sum of two numbers
 function sum(num1,num2){
     if (typeof num1 === 'number' && typeof num2 === 'number') {return num1 + num2;}
-    else {console.log("Please provide numbers or the two input");}
+    else {return "Please provide numbers or the two input";}
 }
 
 // return multiplication of two numbers
 function multiply(num1,num2){
     if (typeof num1 === 'number' && typeof num2 === 'number') {return num1 * num2;}
-    else {console.log("Please provide a number for each input");}
+    else {return "Please provide a number for each input";}
 }
 
 /* If the string is ‘add’, then return the sum of the numbers. If the string is ‘subtract’, return the difference. 
@@ -54,7 +54,7 @@ function calcul(operation,num1,num2) {
         }
         return result;
     }
-    else {console.log("Please provide an operation name ('add' or 'substract' or 'mult' or 'div') and two numbers");}
+    else {return "Please provide an operation name ('add' or 'substract' or 'mult' or 'div') and two numbers";}
 }
 
 //returns the string repeated that many number of times
@@ -68,7 +68,7 @@ function repeatText(text,repeat){
         }
         return returnMessage.join('');
     }
-    else {console.log("Please provide a string as the first input and a number (positive) as a second input");}
+    else {return "Please provide a string as the first input and a number (positive) as a second input";}
 }
 
 // Reverse a string
@@ -80,5 +80,22 @@ function reverse(str){
         }
         return reverseStr.join(''); 
     }
-    else {console.log("Please provide a string as input");}
+    else {return "Please provide a string as input";}
+}
+
+//returns the factorial of a number
+function factorial(num){
+    var result = 1;
+    if (typeof num === 'number') {
+        var numAbs = Math.abs(num); 
+        
+        if (!numAbs) {return result;}
+        else {
+            for (var i = 1 ; i <= numAbs ; i++) {
+                result *= i;
+                }
+            }
+        return result;
+        }
+    else {return "Please provide a number as input";}
 }
