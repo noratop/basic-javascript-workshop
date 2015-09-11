8b2b4a700600fbb5d3bf5e06dd5c92c9538fd109
Nora Top <top.nora@gmail.com>
2015-09-11 18:06:21 +0000

    add function returnLastChar

--- answers.js
+++ answers.js
@@ -2,3 +2,8 @@
 function returnFirstChar(string) {
     return string[0];
 };
+
+// returns the last character of a string
+function returnLastChar(string) {
+    return string[string.length - 1];
+};
